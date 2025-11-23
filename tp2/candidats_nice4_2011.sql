488a77698f5059f2a45104624a82d73e13e518ed
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2025-11-23 14:10:08 +0100

    delete uncessary files after rename of folder

--- tp2/candidats_nice4_2011.sql
+++
@@ -1,3 +0,0 @@
-SELECT DISTINCT Candidat.nom
-FROM Candidat INNER JOIN Candidature ON Candidat.id = Candidature.candidat INNER JOIN Conton ON Candidature.conton = Conton.nom
-WHERE YEAR(Candidature.date) = 2011 AND Conton.nom = "NICE4";
--- tp2/noms_extreme_gauche.sql
+++
@@ -1,3 +0,0 @@
-SELECT DISTINCT Candidat.nom
- FROM Candidat INNER JOIN Parti ON Candidat.parti = Parti.nom
- WHERE Parti.catégorie = "extrème_gauche";
